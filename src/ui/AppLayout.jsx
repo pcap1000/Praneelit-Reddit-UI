fcd8bf0a9438bb647a288b2440b17485239253ca
pcap <praneelchettys@gmail.com>
2025-12-24 17:15:22 +0530

    break the code

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -9,8 +9,8 @@ function AppLayout() {
     {
       id: "1",
       author: "mouthwithnoteeth",
-      postname:"Projects",
-	  Link:'/project-post-1',
+      postname: "Projects",
+      // Link: '/project-post-1',
       title: "Project - Optimizing Time-Slotted Deliveries for Post Offices and Courier Companies Managing time-window-based deliveries has always been a challenge for post offices and courier companies",
       image: "https://miro.medium.com/v2/resize:fit:1400/1*2tsZFK4GIUq1fHX6UpC8xA.jpeg",
       // image: "https://preview.redd.it/once-upon-a-time-on-tatooine-1920x1080-v0-dyn4018f8s3f1.png?width=1080&crop=smart&auto=webp&s=a1b4dafd72177a0cc9334a417dbaa78534459490",
@@ -19,8 +19,8 @@ function AppLayout() {
     {
       id: "2",
       author: "mouthwithnoteeth",
-      postname:"About",
-	  Link:'/about-post-1',
+      postname: "About",
+      // Link: '/about-post-1',
       title: "I’m from Bangalore, born and raised. Can’t say I love this place, but there are parts of it and the people here that I really do. The chaos, the food, the weirdly unpredictable weather—sometimes annoying, but it has its own charm.",
       image: "https://i.pinimg.com/736x/82/73/c2/8273c22a63cc14739f6062753abd2ef7.jpg",
 
